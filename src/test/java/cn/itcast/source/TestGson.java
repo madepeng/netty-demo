1d21b2189c02fce7311fdf8d6ab0a51f03338e7a
madepeng <madepeng@ihuman.com>
2021-12-22 10:32:45 +0800

    初始化

---
+++ src/test/java/cn/itcast/source/TestGson.java
@@ -0,0 +1,12 @@
+package cn.itcast.source;
+
+import cn.itcast.protocol.Serializer;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class TestGson {
+    public static void main(String[] args) {
+        Gson gson = new GsonBuilder().registerTypeAdapter(Class.class, new Serializer.ClassCodec()).create();
+        System.out.println(gson.toJson(String.class));
+    }
+}
